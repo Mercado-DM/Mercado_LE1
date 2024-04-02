c759694870e02a81adeb0df4a8d9211c79a7333e
Mercado-DM <20-04117@g.batstate-u.edu.ph>
2024-04-03 01:50:25 +0800

    Created a function for main menu

--- LabExam1.py
+++ LabExam1.py
@@ -3,5 +3,13 @@
 book_library = {
     'The Stranger' : {'stock' : 125, 'price' : 24},
     'Saudade' : {'stock' : 137, 'price' : 15},
-    ''
+    'Taste Of Sky' : {'stock' : 10, 'price' : 39},
+    'Little Women' : {'stock' : 103, 'price' : 50}
 }
+
+user_acc = {}
+
+def main():
+    print('Welcome to the Bookstore')
+    print('What would you like to do?')
+    print('1. Display available books.')
