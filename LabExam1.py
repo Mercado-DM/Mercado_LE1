5530c43b67c6a237fa995a1a9ce536659736a23f
Mercado-DM <20-04117@g.batstate-u.edu.ph>
2024-04-03 03:06:52 +0800

    added a register function

--- LabExam1.py
+++ LabExam1.py
@@ -9,15 +9,34 @@ book_library = {
 
 user_acc = {}
 
-def main():
-    print('Welcome to the Bookstore')
-    print('What would you like to do?')
-    print('1. Display available books.')
-    print('2. Register User')
-    print('3. Login')
-    print('4. Admin Login')
-    print('5. Exit')
-    choice = int(input('What would you like to do?: '))
 
+def register(user_acc):
+    regname = input("Enter your desired username:")
+    regpassw = input("Enter your desired password:")
+    if regname in user_acc:
+        print('This user already exist.')
+    else:
+        user_acc[regname] = regpassw
+        print(f'Welcome, {regname}')
+
+def main():
+    while True:
+        print('Welcome to the Bookstore')
+        print('What would you like to do?')
+        print('1. Display available books.')
+        print('2. Register User')
+        print('3. Login')
+        print('4. Admin Login')
+        print('5. Exit')
+        choice = int(input('What would you like to do?: '))
+        
+        if choice == 1:
+            print(book_library)
+        elif choice == 2:
+            register(user_acc)
+        elif choice == 5:
+            exit()
+        else:
+            print ("Invalid Input")
 
 main()
